e655b42e850039e5a4e18feec5c5c72e031a4c48
Anwar Hermuche <anwarhermuche2@gmail.com>
2025-07-16 16:43:47 -0300

    feat: added load

---
+++ src/pipeline/load.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+""""
+Transforma um dataframe em um arquivo xlsx
+
+args: dataframe (pd.DataFrame): dataframe a ser transformado
+
+return: None
+"""
+
+def load_to_excel(dataframe: pd.DataFrame, output_path: str) -> None:
+    dataframe.to_excel(output_path, index=False)
+
+
+if __name__ == "__main__":
+    load_to_excel(pd.DataFrame({"col1": [1, 2, 3], "col2": [4, 5, 6]}), "data/output/output.xlsx")
