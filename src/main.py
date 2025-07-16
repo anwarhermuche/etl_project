80d2dab5712943ed52775d9b4e9d981d6cf18151
Anwar Hermuche <anwarhermuche2@gmail.com>
2025-07-16 16:33:38 -0300

    new files

--- src/main.py
+++ src/main.py
@@ -7,5 +7,8 @@ load_dotenv()
 
 if __name__ == "__main__":
     input_file_path = os.environ.get("INPUT_FILE_PATH")
+    if input_file_path is None:
+        raise ValueError("INPUT_FILE_PATH is not set")
+
     files = extract_from_excel(input_file_path)
     print(files)
